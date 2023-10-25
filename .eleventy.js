da25c9c1f8b0f58ee4d6e8fa979e82b6c50a5d5b
Antifantwerp <antifantwerp@proton.me>
2023-10-25 08:11:53 +0200

    Initial Commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const yaml = require("js-yaml");
+const sass = require("sass");
+const eleventySass = require("eleventy-sass");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+
+    eleventyConfig.addPassthroughCopy("src/assets");
+
+    eleventyConfig.addPlugin(eleventySass, {
+        sass: {
+            loadPaths: ["node_modules"]
+        }
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "dist",
+        }
+    }
+}
