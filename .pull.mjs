1b5c146c3fd3ca85c166bac423f25860aeabd9de
Pigeon <antifantwerp@proton.me>
2024-09-23 13:47:57 +0200

    Added .directus.schema.json, expanded cms scripts

--- .pull.mjs
+++
@@ -1,7 +0,0 @@
-import directusToData from "directus-to-data";
-
-directusToData({
-    collectionName: ["AFAStickers", "AFAStickerSources"],
-    outputFilename: "src/_data/{{collectionName}}.json",
-    configFilename: ".directus.json"
-});
