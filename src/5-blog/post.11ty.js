dcc175c810e0a2c6810bbe8719ca05d4eb8c807f
Pigeon <antifantwerp@proton.me>
2024-10-09 10:37:10 +0200

    Improved blog index & post pages

--- src/5-blog/post.11ty.js
+++ src/5-blog/post.11ty.js
@@ -1,5 +1,18 @@
 import markdownit from "markdown-it";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";;
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale("be");
+
 const md = markdownit();
+
+function dateStringToOtherDateString(dateString) {
+    const date = dayjs(dateString).tz("Europe/Brussels");
+    return date.format("D MMMM YYYY, H:mm (UTCZ)");
+}
 
 export const data = {
     layout: "base.pug",
@@ -10,11 +23,17 @@ export const data = {
     },
     permalink: (data) => `blog/${data.post.id}/index.html`,
     title: (data) => `${data.post.title} - Antifantwerp`,
-    description: (data) => data.post.description
+    description: (data) => data.post.description,
+    stylesheet: "/style/blogpost.css"
 }
 
 export function render(data) {
-    const header = `<hgroup><h1>${data.post.title}</h1><p>${data.post.description}</p></hgroup>`
+    const createdAt = dateStringToOtherDateString(data.post.date_created);
+    const updatedAt = dateStringToOtherDateString(data.post.date_updated);
+
+    const header = `<hgroup><h1>${data.post.title}</h1><p>${data.post.description}</p></hgroup>`;
+    const blockquote = `<blockquote>Published at: ${createdAt}<br>Last updated: ${updatedAt}</blockquote>`;
     const content = md.render(data.post.content);
-    return header + content;
+
+    return header + blockquote + content;
 }
