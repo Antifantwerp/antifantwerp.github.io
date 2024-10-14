498d351ab0860b873e146eeacc302e920da312c6
Pigeon <antifantwerp@proton.me>
2024-10-14 21:36:11 +0200

    Moved all watchpigeon.pug data logic to 11tydata.js

--- src/4-watchpigeon/watchpigeon.11tydata.js
+++ src/4-watchpigeon/watchpigeon.11tydata.js
@@ -33,6 +33,7 @@ export default function (configData) {
     // Get data
     const AFAWatchpigeonParties = getDataFile(root, dataDir, "AFAWatchpigeonParties");
     const AFAWatchpigeonArticles = getDataFile(root, dataDir, "AFAWatchpigeonArticles");
+    const AFAWatchpigeonSubjects = getDataFile(root, dataDir, "AFAWatchpigeonArticles");
 
     // Get individual areas
     const individualAreas = new Set();
@@ -46,7 +47,7 @@ export default function (configData) {
         // -- Get area data
         const areaParties = AFAWatchpigeonParties.filter(party => party.areas.includes(areaId));
         const areaPartyIds = areaParties.map((party) => party.id)
-        const areaArticles = AFAWatchpigeonArticles.filter(article => article.responses.find((response) => areaPartyIds.includes(response.party.id)));
+        const areaArticles =  [];  // Do not add values yet, see below  //AFAWatchpigeonArticles.filter(article => article.responses.find((response) => areaPartyIds.includes(response.party.id)));
         // -- Create output & calculation variables
         let pointsByParty = {};
         let maxScaleByParty = {};
@@ -58,12 +59,34 @@ export default function (configData) {
 
         // do this for all articles, in case an organiastion has overlap between areas; all data has to be considered
         AFAWatchpigeonArticles.forEach((article) => {
+            let relevantForArea = false;
+            //AFAWatchpigeonArticles.filter(article => article.responses.find((response) => );
+            // Do the calculations for the percentages later
             article.responses.forEach((response) => {
                 const partyId = response.party.id;
+
                 pointsByParty = addToObjectNumber(pointsByParty, partyId, response.score + 3) // +3 to normalize into positive values from 0-6
                 maxScaleByParty = addToObjectNumber(maxScaleByParty, partyId, 3 + 3)  // Same as above
                 partyArticleCount = addToObjectNumber(partyArticleCount, partyId, 1);
+                
+                if (areaPartyIds.includes(partyId)) {
+                    relevantForArea = true;
+                }
             });
+
+            if (relevantForArea) {
+                const involvedPartyIds = article.responses.map(response => response.party.id)
+                
+                article.parties = AFAWatchpigeonParties.filter((party) => involvedPartyIds.includes(party.id));
+
+                // I'm not going to lie, I'm not quite sure why this if statement ended up being necessary
+                if (Object.keys(article.subjects[0]).includes("AFAWatchpigeonSubjects_id")) {
+                    article.subjects = article.subjects.map(subject => subject.AFAWatchpigeonSubjects_id)                    
+                }
+                article.allTags = article.parties.map(party => party.id).concat(article.subjects.map(subject => subject.id)).join(",");
+
+                areaArticles.push(article);
+            }
         });
 
         // Convert from a possibly negative value to percentages to pass to client-side javascript
