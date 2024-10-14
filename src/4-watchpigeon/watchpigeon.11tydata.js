5b60a0a2b38579144e9c66b68b11cfb1b18c9fd3
Pigeon <antifantwerp@proton.me>
2024-10-14 15:29:44 +0200

    Added getDataFile in watchpigeon data file

--- src/4-watchpigeon/watchpigeon.11tydata.js
+++ src/4-watchpigeon/watchpigeon.11tydata.js
@@ -1,2 +1,18 @@
+import { readFileSync } from "fs";
 
-export const areas = ["flanders", "netherlands", "wallonie"]
+// Eleventy global imports were not working
+function getDataFile(root, dataDir, filename) {
+    // dataDir has a trailing slash
+    const dataPath = `${root}/${dataDir}${filename}.json`
+    return JSON.parse(readFileSync(dataPath, {encoding: "utf-8"}))
+
+}
+
+export default function (configData) {
+    const root = configData.eleventy.env.root;
+    const dataDir = configData.eleventy.directories.data;
+
+    return {
+        areas: ["flanders", "netherlands", "wallonie"]
+    }
+}
